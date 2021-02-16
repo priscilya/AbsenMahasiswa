dcd9d20a790a83897cf5f4f138a0c8ef5bc02706
priscilya <priscilyachristiana15@gmail.com>
2021-02-16 13:44:23 +0700

    commit 2

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/Home', function () {
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/Home', function () {      
     return view('Home');
 });
 
